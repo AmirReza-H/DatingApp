55498eb645b5e6b08d4c375a93bc78b24b337347
unknown <you@example.coamirreza.hoseyn13@gmail.com>
2019-04-21 17:04:12 +0430

    End of Section 10

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -20,6 +20,7 @@ namespace DatingApp.API.Helpers
                 opt.ResolveUsing(src =>src.DateOfBirth.CalculateAge());
             });
             CreateMap<Photo,PhotoForDetaileDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
